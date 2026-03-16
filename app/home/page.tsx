89a7cb09379d485ce387441a138c4b83492b4631
kunalkarpe <kunalkarpe32@gmail.com>
2026-03-16 14:37:14 +0530

    feat : client : added everf frontend file and folder in client main folder

--- app/home/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-    return (
-        <div className="">
-            Hey from Home
-        </div>
-    )
-}
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className=" w-full ">
-      <p className="text-primary">Hey frmom home</p>
-    </div>
-  );
-}
